34497ccd8b1cbf57158eee65a21bfe3941bfa7c0
seungineer <97353541+seungineer@users.noreply.github.com>
2024-04-03 23:17:52 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 127540 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,18 @@
 n = int(input())
+timetable = []
 
-time_table = []
-for i in range(n):
+for _ in range(n):
     st, en = map(int, input().split())
-    # 시작, 지속시간, 끝[ [1, 3, 4], [3, 2, 5], ... , [12, 2, 14] ]
-    time_table.append([st, en])
+    timetable.append([st, en])
 
-time_table.sort(key=lambda x :(x[1],x[0])) #끝나는 시간, 시작 시간 기준으로 오름차순 정렬
+timetable.sort(key= lambda x: (x[1], x[0]))
 
-cnt = 0
-last_endtime = 0
+end = timetable[0][1]
+cnt = 1
 
-for i in range(len(time_table)):
-
-    if last_endtime <= time_table[i][0]: # 지난 회의 끝 시점 <= 지금 회의 시작 시점
-        last_endtime = time_table[i][1]
-        cnt += 1 # 횟수 추가
+for i in range(1, len(timetable)):
+    if timetable[i][0] >= end:
+        end = timetable[i][1]
+        cnt += 1
+        
 print(cnt)
