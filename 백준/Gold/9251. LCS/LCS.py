6509d5b37a87fd1e5a4db9e4859621085c8961cc
seungineer <97353541+seungineer@users.noreply.github.com>
2024-04-03 12:55:10 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 118332 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,13 +1,14 @@
-str1 = list(input().strip())
-str2 = list(input().strip())
+str1 = input().strip()
+str2 = input().strip()
 
-lcs = [[0] * (len(str2)+1) for _ in range(len(str1) + 1)]
+dp = [[0 for _ in range(len(str2)+1)] for _ in range(len(str1)+1)]
+dp[0][0] = 0
 
 for i in range(1, len(str1)+1):
     for j in range(1, len(str2)+1):
         if str1[i-1] == str2[j-1]:
-            lcs[i][j]= lcs[i-1][j-1] + 1
+            dp[i][j] =dp[i-1][j-1]+ 1
         else:
-            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-print(lcs[len(str1)][len(str2)])
+print(dp[len(str1)][len(str2)])
