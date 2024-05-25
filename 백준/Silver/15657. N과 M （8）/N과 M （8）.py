3933f63ee4af99a26002b7e2ec6cac90dc459fb4
seungineer <97353541+seungineer@users.noreply.github.com>
2024-05-25 23:58:15 +0900

    [Silver III] Title: N과 M (8), Time: 196 ms, Memory: 112888 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+
+
+
+q = []
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+
+def dfs(l):
+    if len(q) == M:
+        print(' '.join(map(str, q)))
+        return
+
+    for i in range(l, N):
+        q.append(a[i])
+        dfs(i)
+        q.pop()
+        
+dfs(0)
