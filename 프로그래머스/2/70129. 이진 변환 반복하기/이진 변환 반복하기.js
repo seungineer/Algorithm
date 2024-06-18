79abd26b51df5df140adee5d873b7cb2b68694d7
seungineer <97353541+seungineer@users.noreply.github.com>
2024-06-19 00:44:43 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.81 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,15 +1,4 @@
 function solution(s) {
-    let sum = 0
-    let cnt = 0
-    while (s != '1'){
-        let arr_s = [...s]
-        a = arr_s.length
-        let filtered = arr_s.filter((value) => {return value==='1'})
-        b = filtered.length
-        sum += (a - b)
-        cnt += 1
-        s = b.toString(2)
-    }
-    
-    return [cnt, sum];
+    var answer = [];
+    return answer;
 }
