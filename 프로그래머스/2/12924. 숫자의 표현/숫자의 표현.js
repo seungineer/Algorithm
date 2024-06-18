96afd0d737f547f56d70cfa8111b22b52e4bb0ff
seungineer <97353541+seungineer@users.noreply.github.com>
2024-06-19 02:00:43 +0900

    [level 2] Title: 숫자의 표현, Time: 2.37 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,17 +1,4 @@
 function solution(n) {
-    let cnt = 1
-    let num = 2
-    let a = 0
-    while (1) {
-        a = (num-1)*(num)/2
-        if (a>=n){
-            break
-        }
-        if (Math.floor((n - a)%num) === 0){
-            cnt += 1
-        }
-        num += 1
-    }
-    console.log(cnt)
-    return cnt
+    var answer = 0;
+    return answer;
 }
