da7075ebf05177802f49b7fd7c75e45d15d4ab69
seungineer <97353541+seungineer@users.noreply.github.com>
2024-08-09 16:28:46 +0900

    [Silver II] Title: 촌수계산, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,26 @@
+n = int(input())
+st, en = map(int, input().split())
+lines_cnt = int(input())
+matrix = [[] for _ in range(n+1)] # 사람 번호를 인덱스로 사용
+for i in range(lines_cnt):
+    a, b = map(int, input().split())
+    matrix[a].append(b)
+    matrix[b].append(a)
+vis = [False for _ in range(n+1)]
+
+# 100log100 회 recursion
+res = -1
+def dfs(node, length):
+    global res
+    if node == en:
+        res = length
+        return
+    
+    for k in matrix[node]:
+        if not vis[k]:
+            vis[k] = True
+            dfs(k, length+1)
+vis[st] = True
+dfs(st, 0)
+
+print(res)
