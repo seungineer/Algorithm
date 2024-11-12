8376c89ac2fbd5e2d7c5efb70ef8306ef0e81cc4
seungineer <97353541+seungineer@users.noreply.github.com>
2024-11-12 18:01:39 +0900

    [Gold III] Title: 두 배열의 합, Time: 420 ms, Memory: 200464 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -0,0 +1,40 @@
+# 어짜피 개수만 구하면 되니까
+# A의 부분으로 x를 만들 수 있다 / B의 부분으로 y를 만들 수 있다
+# x가 땡땡이면 y는 T - x로 고정된다.
+# A 정렬 후에 누적합 / B 정렬 후에 누적합
+# T를 만들어야 하니까, A가 몇일 때, B가 몇이어야 한다.
+
+# A가 몇인 경우의 수를 좍 센 다음에
+# A가 몇일 때, B가 몇인지 곱해서 토탈 구하면 될 거 같다.
+
+T = int(input())
+N = int(input())
+lst_a = list(map(int, input().split()))
+M = int(input())
+lst_b = list(map(int, input().split()))
+
+def find(acc, idx, lst, part_sum):
+    acc += lst[idx]
+    if acc in part_sum.keys(): part_sum[acc] += 1
+    else: part_sum[acc] = 1
+
+    for j in range(idx+1, len(lst)):
+        find(acc, j, lst, part_sum)
+        break
+
+part_sum_a = dict()
+for i in range(N):
+    find(0, i, lst_a, part_sum_a)
+part_sum_b = dict()
+for i in range(M):
+    find(0, i, lst_b, part_sum_b)
+answer = 0
+for key_a in part_sum_a.keys():
+    target = T - key_a
+    if target in part_sum_b.keys():
+        cnt_b = part_sum_b[target]
+    else: continue
+    cnt_a = part_sum_a[key_a]
+    answer += (cnt_a * cnt_b)
+
+print(answer)
