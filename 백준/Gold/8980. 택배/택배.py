843f60d564234a91681b77e1294f2ad7658df4b6
seungineer <97353541+seungineer@users.noreply.github.com>
2024-11-21 18:25:35 +0900

    [Gold I] Title: 택배, Time: 164 ms, Memory: 111768 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -3,19 +3,21 @@ M = int(input())
 info = []
 for _ in range(M):
     st, en, w = map(int, input().split())
-    info.append([en-st-1, -w, st])
+    info.append([en, -w, st])
 info.sort()
 boxCount = [C for _ in range(N+1)]
 answer = 0
-while info:
-    diff, minus_weight, start = info.pop(0)
+for j in range(M):
+    affordWeight = 1e9
+    end, minus_weight, start = info[j]
     weight = - minus_weight
-
-    affordWeight = min(boxCount[start:start + diff + 1])
+    for k in range(start, end):
+        affordWeight = min(affordWeight, boxCount[k])
+    
     if affordWeight < weight: weight = affordWeight
     if weight != 0:
         answer += weight
-        for i in range(start, start + diff + 1):
+        for i in range(start, end):
             boxCount[i] -= weight
         
 print(answer)
