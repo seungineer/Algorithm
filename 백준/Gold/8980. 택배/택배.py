1164db5407d1d023f815db661c7e9993c6658c8f
seungineer <97353541+seungineer@users.noreply.github.com>
2024-11-21 17:02:51 +0900

    [Gold I] Title: 택배, Time: 92 ms, Memory: 108080 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Gold/8980. 택배/택배.py
@@ -0,0 +1,21 @@
+N, C = map(int, input().split())
+M = int(input())
+info = []
+for _ in range(M):
+    st, en, w = map(int, input().split())
+    info.append([en-st-1, -w, st])
+
+boxCount = [C for _ in range(N+1)]
+answer = 0
+while info:
+    diff, minus_weight, start = info.pop(0)
+    weight = - minus_weight
+
+    affordWeight = min(boxCount[start:start + diff + 1])
+    if affordWeight < weight: weight = affordWeight
+    if weight != 0:
+        answer += weight
+        for i in range(start, start + diff + 1):
+            boxCount[i] -= weight
+        
+print(answer)
