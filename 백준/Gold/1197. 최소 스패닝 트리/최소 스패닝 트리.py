653de6aed098a938dc24747d476687c2fc601603
seungineer <97353541+seungineer@users.noreply.github.com>
2024-12-11 08:04:11 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 520 ms, Memory: 147432 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,39 +1,31 @@
-import sys
-read = sys.stdin.readline
+import heapq as hq
+V, E = map(int, input().split())
+edges = dict()
+for _ in range(E):
+    st, en, cost = map(int, input().split())
+    if st in edges: edges[st].append([en, cost])
+    else: edges[st] = [[en, cost]]
+    if en in edges: edges[en].append([st, cost])
+    else: edges[en] = [[st, cost]]
 
-v, e = map(int, read().split())
-parent_lst = [0] * (v+1)
-for i in range(1, v+1): # 각 노드의 parent list 생성
-    parent_lst[i] = i
+vis = [0 for _ in range(V+1)]
+waits = []
+vis[1] = 1
+for next_node, cost in edges[1]:
+    hq.heappush(waits, [cost, next_node])
+answer = 0
+cnt = 0
+while waits:
+    cost, node = hq.heappop(waits)
+    if vis[node] == 1: continue
 
-def findParent(parent_lst, x): # parent list에서 x의 부모 노드 찾기
-    if parent_lst[x] != x: # 부모 노드 = 자식 노드인 경우
-        parent_lst[x] = findParent(parent_lst, parent_lst[x]) # 부모 노드 != 자식 노드인 경우, 부모 노드가 찐 부모 노드인지 확인하러 감
-    return parent_lst[x]
+    vis[node] = 1
+    answer += cost
+    cnt += 1
+    if cnt == V - 1: break
 
-def unionAB(parent_lst, x, y): # x, y를 합집합으로(=하나의 공통된 부모 노드를 갖도록) 연결하는 것 
-    a = findParent(parent_lst, x)
-    b = findParent(parent_lst, y)
-    if a < b:
-        parent_lst[b] = a
-    else:
-        parent_lst[a] = b
+    for next_node, cost in edges[node]:
+        if vis[next_node] == 0:
+            hq.heappush(waits, [cost, next_node])
 
-edges = []
-tot = 0
-
-for _ in range(e):
-    a, b, cost = map(int, read().split())
-    edges.append((cost, a, b))
-
-edges.sort() # cost 기준으로 오름차순 sorting
-
-for i in range(e):
-    cost, a, b = edges[i]
-
-    if findParent(parent_lst, a) != findParent(parent_lst, b) :
-        unionAB(parent_lst, a, b)
-        tot += cost
-
-print(tot)
-
+print(answer)
