97b77a3098f1dca6aa12235b0542ce673bcdb282
seungineer <97353541+seungineer@users.noreply.github.com>
2025-01-09 01:45:06 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+N = int(input())
+
+def isPrime(checkNumber):
+    for i in range(2, int(checkNumber**(0.5) + 1)):
+        if checkNumber % i == 0: return False
+    
+    return True
+
+def bt(number):
+    if len(str(number)) == N:
+        print(number)
+        return
+    
+    for i in range(1, 10):
+        if i % 2 == 0: continue
+        
+        next_cand = number * 10 + i
+        if isPrime(next_cand):
+            bt(next_cand)
+
+for pos in range(9):
+    if pos in [2, 3, 5, 7]:
+        bt(pos)
