1c16b6b77493f2d66c6ef95656d116650c805e61
seungineer <97353541+seungineer@users.noreply.github.com>
2025-02-19 02:41:18 +0900

    [Silver V] Title: 집합, Time: 920 ms, Memory: 126892 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,32 +1,31 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-S = set()
-for _ in range(n):
-    cl = input().split()
-    if cl[0] == "all":
-        S = {1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-        continue
-    if cl[0] == "empty":
-        S = set()
-        continue
-    command = cl[0]
-    k = int(cl[1])
-    
-    if command == "add":
-        S.add(k)
-        continue
-    if command == "remove":
-        S.discard(k)
-        continue
-    if command == "check":
-        if k in S:
-            print(1)
+def solution():
+    N = int(input())
+    S = 0b0
+    for _ in range(N):
+        inputs = input().rstrip().split()
+        if len(inputs) == 2:
+            command, num = inputs[0], int(inputs[1])
         else:
-            print(0)
-    if command == "toggle":
-        if k in S:
-            S.discard(k)
-        else:
-            S.add(k)
-    
+            command = inputs[0]
+        if command == 'all':
+            S = 0b111111111111111111111
+        if command == 'add':
+            S = S | (0b1 << num)
+        if command == 'check':
+            if S & (0b1 << num) : print(1)
+            else: print(0)
+        if command == 'remove':
+            if S & 0b1 << num:
+                S = S ^ (0b1 << num)
+        if command == 'toggle':
+            if S & 0b1 << num:
+                S = S ^ (0b1 << num)
+            else:
+                S = S | (0b1 << num)
+        if command == 'empty':
+            S = 0b0
+
+        
+solution()
