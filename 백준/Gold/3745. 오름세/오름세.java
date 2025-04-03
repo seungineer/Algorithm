90009a48afe8d79f4fdc96ed7b842f52f71dc0a6
seungineer <97353541+seungineer@users.noreply.github.com>
2025-04-03 22:53:00 +0900

    [Gold II] Title: 오름세, Time: 312 ms, Memory: 28368 KB -BaekjoonHub

--- 백준/Gold/3745. 오름세/오름세.java
+++ 백준/Gold/3745. 오름세/오름세.java
@@ -1,55 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr, dp;
 
-	static int[] arr, dp;
-	public static void main(String[] args){
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));){
-			String line = null;
-			while((line = br.readLine()) != null) {
-				int n = Integer.parseInt(line.trim());
-				arr = new int[n];
-				dp = new int[n+1];
-				
-				st = new StringTokenizer(br.readLine());
-				for (int i = 0; i < n; i++) {
-					arr[i] = Integer.parseInt(st.nextToken());
-				}
-			
-				int len = 0;
-				int idx = 0;
-				for (int i = 0; i < n; i++) {
-					if(arr[i] > dp[len]) {
-						dp[++len] = arr[i];
-					}else {
-						idx = lowerbound(0, len, arr[i]);
-						dp[idx] = arr[i];
-					}
-				}
-				sb.append(len+"\n");
-			}
-			
-		} catch(Exception e ) {
-			
-		} finally {
-			System.out.println(sb.toString());
-		}
-		
-	}
-	
-	static int lowerbound(int s, int e, int key) {
-		while(s < e) {
-			int mid = (s+e)/2;
-			
-			if(dp[mid] >= key) {
-				e = mid;
-			}else {
-				s = mid+1;
-			}
-		}
-		return e;
-	}
+    static int lowerbound(int s, int e, int target) {
+        while (s < e) {
+            int mid = (s+e)/2;
+            if(dp[mid] >= target) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return e;
+    }
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                int n = Integer.parseInt(line.trim());
+                arr = new int[n];
+                dp = new int[n+1];
+
+                st = new StringTokenizer(br.readLine());
+                for (int i=0; i < n; i++) {
+                    arr[i] = Integer.parseInt(st.nextToken());
+                }
+
+                int len = 0;
+                int idx = 0;
+                for (int i = 0; i<n; i++) {
+                    if (arr[i] > dp[len]) {
+                        dp[++len] = arr[i];
+                    }
+                    else {
+                        idx = lowerbound(0, len, arr[i]);
+                        dp[idx] = arr[i];
+                    }
+                }
+                sb.append(len+"\n");
+            }
+        } catch(Exception e) {
+        } finally {
+            System.out.println(sb.toString());
+        }
+
+
+    }
 }
