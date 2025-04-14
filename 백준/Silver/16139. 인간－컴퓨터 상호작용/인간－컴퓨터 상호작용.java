3e10e6c908c3c0142e9af8ffdcbe1f6d0f7f3d0b
seungineer <97353541+seungineer@users.noreply.github.com>
2025-04-14 23:34:20 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 828 ms, Memory: 136500 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,20 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static char[] word;
     static int q;
-    static int[][] cnts;
+    static long[][] cnts;
     public static void main(String[] args) throws IOException {
 //        문자 순회하면서 각 자리에 어떤 알파벳이 있는지 체크
 //        체크된 배열을 기준으로 누적합 만들기
 //        각 알파벳별 누적합
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         word = br.readLine().toCharArray();
-        cnts = new int[word.length+1][26];
+        cnts = new long[word.length+1][26];
 
         for (int i=1; i<word.length+1; i++) {
             int ord = (int)word[i-1] - (int)'a';
@@ -34,7 +33,9 @@ public class Main {
             int r = Integer.parseInt(st.nextToken());
             l++;
             r++;
-            System.out.println(cnts[r][(int)alpha-(int)'a'] - cnts[l-1][(int)alpha-(int)'a']);
+            bw.write((cnts[r][(int)alpha-(int)'a'] - cnts[l-1][(int)alpha-(int)'a']) + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
