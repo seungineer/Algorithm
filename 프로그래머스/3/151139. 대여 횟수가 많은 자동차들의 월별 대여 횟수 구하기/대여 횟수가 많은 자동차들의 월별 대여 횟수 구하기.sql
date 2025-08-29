dc923edbf59e9a98014f0171bdcb556481648f0e
seungineer <97353541+seungineer@users.noreply.github.com>
2025-08-29 23:52:09 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,8 +1,11 @@
--- 코드를 입력하세요
-SELECT MONTH(START_DATE) AS MONTH, CAR_ID, COUNT(*) AS RECORDS
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE CAR_ID IN (SELECT CAR_ID
-                    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                    WHERE START_DATE LIKE '2022-8___' AND END_DATE LIKE '2022-10___'
-                    GROUP BY CAR_ID
-                   HAVING COUNT(*) >= 5)
+select month(start_date) as month, car_id, count(*) as records
+from car_rental_company_rental_history as o
+where o.car_id in (
+    select car_id
+    from car_rental_company_rental_history
+    where month(start_date) >= 8 and month(start_date) <= 10 and year(start_date) = 2022
+    group by car_id
+    having count(*) >= 5
+) and month(start_date) >= 8 and month(start_date) <= 10 and year(start_date) = 2022
+group by car_id, month(start_date)
+order by month asc, car_id desc
