981055c26c390a5f38bb6517661d9fa88e5f2a4f
seungineer <97353541+seungineer@users.noreply.github.com>
2025-10-30 13:55:33 +0900

    [level 3] Title: 입국심사, Time: 539.22 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
 
 ### 성능 요약
 
-메모리: 13.3 MB, 시간: 557.66 ms
+메모리: 13.3 MB, 시간: 539.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 21:59:56
+2025년 10월 30일 13:55:27
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,20 +1,25 @@
 def solution(n, times):
-    maxTime = max(times)
-    maxWait = maxTime * n
+    answer = int(1e18)
+    # 기다리는 사람 수보다 더 많이 검사 가능한 경우
+    ## 시간 줄이기
+    
+    # 기다리는 사람 수보다 더 적게 검사 가능한 경우
+    ## 시간 늘리기
     
     l = 1
-    r = maxWait
-    answer = maxWait
+    r = int(1e18)
     while l <= r:
         mid = (l + r) // 2
+        
         cnt = 0
         for t in times:
             cnt += mid // t
         
-        if cnt < n:
-            l = mid + 1
-        else:
+        if cnt >= n:
             answer = min(answer, mid)
             r = mid - 1
-
+        else:
+            l = mid + 1
+            
+    
     return answer
