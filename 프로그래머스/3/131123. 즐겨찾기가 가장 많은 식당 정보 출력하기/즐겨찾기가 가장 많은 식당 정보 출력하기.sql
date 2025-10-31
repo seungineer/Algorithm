36e2dc2907534ff7efc1d99ff89814e377f0f824
seungineer <97353541+seungineer@users.noreply.github.com>
2025-10-31 16:45:12 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,10 +1,8 @@
--- 음식종류, max_favorite인 서브쿼리
--- 해당 서브쿼리 내에 포함된 것 출력
--- 음식 종류를 기준으로 내림차순 정렬
-select FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
-from REST_INFO
-where (FOOD_TYPE, FAVORITES) IN
-(select food_type, MAX(favorites)
-from REST_INFO
-group by food_type)
+select food_type, rest_id, rest_name, favorites
+from rest_info
+where (food_type, favorites) in (
+    select food_type, max(favorites) as favorites
+    from rest_info
+    group by food_type
+)
 order by 1 desc
